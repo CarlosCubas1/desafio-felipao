c47b24bd7257934458571002e990a720a0c7d73b
Carlos Cubas <carloscubas1.hc@gmail.com>
2024-01-13 17:59:20 -0300

    alterando variaveis else if

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 //Desafio Classificador de nível de Herói
 let nickHeroi = "Cubas"
-let nivelXp = 1.000
+let nivelXp = 10.000
 
   if (nivelXp <=1.000 ){
         xp = "ferro"
-}  if (nivelXp >=1.001 && nivelXp <=11.000){
+}else if (nivelXp >=1.001 && nivelXp <=2.000){
     xp = "Bronze"
-}if (nivelXp >=1.001 && nivelXp <=2.000){
+}else if (nivelXp >=2.001 && nivelXp <=3.000){
     xp = "Prata"
-}if (nivelXp >=2.001 && nivelXp <=5.000){
+}else if (nivelXp >=3.001 && nivelXp <=5.000){
     xp = "Ouro"
-}if (nivelXp >=5.001 && nivelXp <=7.000){
+}else if (nivelXp >=5.001 && nivelXp <=7.000){
     xp = "Platina"
-}if (nivelXp >=7.001 && nivelXp <=8.000){
+}else if (nivelXp >=7.001 && nivelXp <=8.000){
     xp = "Ascendente"
-}if (nivelXp >=8.001 && nivelXp <=9.000){
+}else if (nivelXp >=8.001 && nivelXp <=9.000){
     xp = "Imortal"
-}if (nivelXp >=9.001 && nivelXp <=10.000){
+}else if (nivelXp >=9.001 && nivelXp <=10.000){
     xp = "Radiante"
 }
 console.log("O Herói de nome", (nickHeroi), "está no nível de" ,(xp))
